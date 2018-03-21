00ff15861c6482c7fcd409dfd6fbea161c8d145d
Peter Watkins <Peter.Watkins@csiro.au>
2018-03-21 14:22:06 +1100

    Initial form

--- scripts/data_wrangling.R
+++ scripts/data_wrangling.R
@@ -1,4 +1,7 @@
 library(tidyverse)
-
 legume <- read_csv("data/legume_weights_rhizobia_20str.csv")
 
+legume %>% 
+  mutate(Block = as.factor(Block)) %>% ## blocks represent experimental replication
+  mutate(Plant = as.factor(Plant)) %>% 
+  mutate(Strain = as.factor(Strain)) 
